a22305d4d24369accb0c15f7ee3007dd09f1d719
scott <aidan.s.hill@gmail.com>
2023-03-21 21:22:26 -0700

    new formatting , removed retarded alice in wonderland shit

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 18, 2022 00:00:00").getTime();
-// Update the count down every 1 second
-var x = setInterval(function() {
-    
-    // document.getElementById('users').innerText = 'ERR';
+const countDownDate = new Date("Feb 11, 2022 00:00:00").getTime();
+// Get today's date and time
+const now = new Date().getTime();
+// someones special day
+const amp_day = new Date("Aug 18, 2022 00:00:00").getTime();
+// Update the countdown every 1 second
 
+function ObtainTime(distance) {
+    distance = Math.abs(distance);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    var curHour = Math.floor((now % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var curMinutes = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
-    var curSeconds = Math.floor((now % (1000 * 60)) / 1000);
-    var dynamicSeconds = curSeconds;
-    var dynamicMinutes = curMinutes;
+    const curHour = Math.floor((now % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const curMinutes = Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
+    const curSeconds = Math.floor((now % (1000 * 60)) / 1000);
+    let dynamicSeconds = curSeconds;
+    let dynamicMinutes = curMinutes;
     if (curSeconds < 10) {
-    dynamicSeconds = "0" + curSeconds;
+        dynamicSeconds = "0" + curSeconds;
     }
     if (curMinutes < 10) {
-    dynamicMinutes = "0" + curMinutes;
+        dynamicMinutes = "0" + curMinutes;
     }
+    return days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+}
+
+const x = setInterval(function () {
+
+    let days_together = ObtainTime(countDownDate - now);
+    let adult_time = ObtainTime(amp_day - now);
 
     // Display the result in the element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days_together
+    document.getElementById("countdown2").innerHTML = adult_time
     //document.getElementById("currentTime").innerHTML = curHour + " : " + dynamicMinutes + " : " + dynamicSeconds;
-    // If the count down is finished, write some text
-    if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "the birthday bash starts now";
-    }
+    // If the countdown is finished, write some text
 }, 1000);
 
 //"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 
-function getIP(json) {
-    var e = document.getElementById("ip");
+window.onload = function() {
+    const e = document.getElementById("ip");
     e.innerHTML = json.ip;
 }
 
@@ -60,8 +58,8 @@ else {
 }
 };
 function showPosition(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
     document.getElementById("latitude").innerHTML = lat.toFixed(5);
     document.getElementById("longitude").innerHTML = lon.toFixed(5);
 }
